abec45dfb21ccedd64613ad43738a65d5012c323
romain.cian <romain.cian@kalixys.com>
2022-09-20 22:17:19 +0200

    Oui

--- src/partials/Hero.tsx
+++ src/partials/Hero.tsx
@@ -10,7 +10,7 @@ const Hero = () => (
     <HeroAvatar
       title={
         <>
-          Hi there, I'm <GradientText>Ixartz</GradientText> 👋
+          Hi there, I'm <GradientText>Weltanschauung</GradientText> 👋
         </>
       }
       description={
